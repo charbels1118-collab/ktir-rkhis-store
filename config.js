a50bcba9ef86ce6face791f25ccf4b0712f0c3e3
charbels1118-collab <charbels1118@gmail.com>
2026-08-18 09:41:58 +0300

    Configure store WhatsApp checkout

---
+++ config.js
@@ -0,0 +1,7 @@
+window.STORE_CONFIG = {
+  storeName: "Ktir rkhis",
+  whatsappNumber: "96176861904",
+  currency: "$",
+  deliveryTextEn: "Delivery available all over Lebanon",
+  deliveryTextAr: "التوصيل متوفر إلى جميع أنحاء لبنان"
+};
